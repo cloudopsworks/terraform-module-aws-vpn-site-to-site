ec2afc378cfad41fab41aec41ffc344501789247
Cristian Beraha <cristian@cloudops.works>
2025-06-30 17:15:21 -0300

    Update VPN tunnel alarms to support dynamic SNS topic resolution
    Enhances CloudWatch alarms for VPN tunnel status by introducing dynamic resolution of SNS topics via `aws_sns_topic` data source. Simplifies action handling and improves configurability.

--- alarms.tf
+++ alarms.tf
@@ -4,24 +4,29 @@
 #            Distributed Under Apache v2.0 License
 #
 
+data "aws_sns_topic" "vpn_status" {
+  count = try(var.settings.alarms.sns_topic, "") != "" && try(var.settings.alarms.enabled, false) ? 1 : 0
+  name  = var.settings.alarms.sns_topic
+}
+
 resource "aws_cloudwatch_metric_alarm" "tunnel1_status" {
-  count = try(var.settings.alarm.enabled, false) ? 1 : 0
+  count               = try(var.settings.alarms.enabled, false) ? 1 : 0
   alarm_name          = "${local.name} Tunnel 1 VPN Status"
   comparison_operator = "LessThanToThreshold"
   evaluation_periods  = "1"
   threshold           = "0.5"
   datapoints_to_alarm = "1"
   alarm_description   = "${local.name} VPN Status - Address: ${aws_vpn_connection.this.tunnel1_address} - Alarm if the VPN connection is not healthy"
-  alarm_actions = [
-    var.settings.alarms.sns_topic_arn,
-  ]
-  ok_actions = [
-    var.settings.alarms.sns_topic_arn,
-  ]
+  alarm_actions = flatten([
+    try([data.aws_sns_topic.vpn_status[0].arn], [var.settings.alarms.sns_topic_arn], [])
+  ])
+  ok_actions = flatten([
+    try([data.aws_sns_topic.vpn_status[0].arn], [var.settings.alarms.sns_topic_arn], [])
+  ])
   metric_name = "TunnelState"
   namespace   = "AWS/VPN"
   dimensions = {
-    VpnId = aws_vpn_connection.this.id
+    VpnId           = aws_vpn_connection.this.id
     TunnelIpAddress = aws_vpn_connection.this.tunnel1_address
   }
   treat_missing_data        = "notBreaching"
@@ -30,23 +35,23 @@ resource "aws_cloudwatch_metric_alarm" "tunnel1_status" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "tunnel2_status" {
-  count = try(var.settings.alarm.enabled, false) ? 1 : 0
+  count               = try(var.settings.alarms.enabled, false) ? 1 : 0
   alarm_name          = "${local.name} Tunnel 2 VPN Status"
   comparison_operator = "LessThanToThreshold"
   evaluation_periods  = "1"
   threshold           = "0.5"
   datapoints_to_alarm = "1"
   alarm_description   = "${local.name} VPN Status - Address: ${aws_vpn_connection.this.tunnel2_address} - Alarm if the VPN connection is not healthy"
-  alarm_actions = [
-    var.settings.alarms.sns_topic_arn,
-  ]
-  ok_actions = [
-    var.settings.alarms.sns_topic_arn,
-  ]
+  alarm_actions = flatten([
+    try([data.aws_sns_topic.vpn_status[0].arn], [var.settings.alarms.sns_topic_arn], [])
+  ])
+  ok_actions = flatten([
+    try([data.aws_sns_topic.vpn_status[0].arn], [var.settings.alarms.sns_topic_arn], [])
+  ])
   metric_name = "TunnelState"
   namespace   = "AWS/VPN"
   dimensions = {
-    VpnId = aws_vpn_connection.this.id
+    VpnId           = aws_vpn_connection.this.id
     TunnelIpAddress = aws_vpn_connection.this.tunnel2_address
   }
   treat_missing_data        = "notBreaching"
